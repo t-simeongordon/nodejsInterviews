a804de934c4df703e5fd86033efdffeb6f7e1d8e
tsg <tsg@tsgs-MacBook-Pro.local>
2024-03-19 15:06:44 +0000

    v0: forloop added

---
+++ interviewQuestions/validateSubsequence/forloop.js
@@ -0,0 +1,21 @@
+const array = [5, 1, 22, 25, 6, -1, 8, 10];
+const sequence = [0, 6, -1, 10];
+
+function isValidSubsequence(array, sequence) {
+    let seqIdx = 0 
+    for(i=0; i<array.length; i++){  
+      if(array[i]===sequence[seqIdx]){
+        seqIdx++
+      }
+  
+      if(seqIdx===sequence.length){
+        return true
+      }
+    }
+  
+    return false
+  }
+
+  const res = isValidSubsequence(array, sequence)
+  console.log("isValidSubsequence")
+  console.log(res)
